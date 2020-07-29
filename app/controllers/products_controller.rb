41fdc751c8c045a0bcca7e32c735db54a7be826c
Denrig <bogdanboer99@gmail.com>
2020-07-29 14:55:56 +0300

    Styled the new_product from

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,13 +4,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @product ||= Product.new
   end
 
   def create
-    product = Product.new(product_params)
-
-    redirect_to products_url if product.save
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
   end
 
   private
