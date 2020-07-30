8cd90a96bdc1c369c59bcc8061f6c355d80f8321
Denrig <bogdanboer99@gmail.com>
2020-07-30 13:12:02 +0300

    Empty Project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,2 @@
 module ApplicationHelper
-  CONTACTS = {
-    email: 'email@email.com',
-    phone: '1234567890',
-    fax: '21451341231512'
-  }.freeze
-
-  SCHEDULE = {
-    sunday: '10PM-10AM',
-    monday: '10PM-10AM',
-    tuesday: '10PM-10AM',
-    wednesday: '10PM-10AM',
-    thursday: '10PM-10AM',
-    friday: '10PM-10AM',
-    saturday: '10PM-10AM'
-  }.freeze
-
-  MAIN_TEXT = "Maecenas vells at magna sagittis venenatis. Integer pretium auctor magna, egestas justo gravida vitae.
-  In lacinia auctor auctor".freeze
-
-  DESCRIPTION = 'This is a short description of our restaurant because why not '.freeze
 end
