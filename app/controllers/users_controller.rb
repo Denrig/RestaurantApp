b1ffc891a0b8aae718c8efc4c4e57ecef115fc48
Denrig <bogdanboer99@gmail.com>
2020-08-04 16:56:18 +0300

    Erros now appear on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    new_user=User.new(user_params)
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name,:email,:password,:password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
