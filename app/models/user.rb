c85911adb4fc80eb732a12c330a79c071cb1e604
Denrig <bogdanboer99@gmail.com>
2020-08-04 10:49:51 +0300

    Added password to user and created tests for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
   VALID_NAME_REGEX = /\A[a-zA-Z ]+\z/.freeze
   validates :name, presence: true, length: { maximum: 50, minimum: 8 }, format: { with: VALID_NAME_REGEX }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
