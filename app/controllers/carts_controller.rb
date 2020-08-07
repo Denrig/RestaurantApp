4946de89a789468cc9637cb91acd956418e4350f
Denrig <bogdanboer99@gmail.com>
2020-08-07 15:29:39 +0300

    Displayed total price of cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   def show
     @products_carts = Cart.find_by(id: params[:id]).products_carts
+    @total_price = @products_carts.map { |element| element.product.price * element.quantity }.sum
   end
 
   def add
@@ -10,9 +11,14 @@ class CartsController < ApplicationController
     # If the product it's not in the cart add it else increment its quantity
     if product_cart.nil?
       product_cart = ProductsCart.new(product: product, cart: cart, quantity: 1)
-      flash[:danger] = 'Something went wrong while adding to cart' unless product_cart.save
+      if product_cart.save
+        flash.now[:sucess] = 'Added to cart'
+      else
+        flash.now[:danger] = 'Something went wrong while adding to cart'
+      end
     else
       product_cart.update(quantity: product_cart.quantity + 1)
     end
+    redirect_to products_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Validations
   validates :password, presence: true, length: { minimum: 6 }
   VALID_NAME_REGEX = /\A[a-zA-Z ]+\z/.freeze
-  validates :name, presence: true, length: { maximum: 50, minimum: 8 }, format: { with: VALID_NAME_REGEX }
+  validates :name, presence: true, length: { minimum: 8, maximum: 50 }, format: { with: VALID_NAME_REGEX }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
