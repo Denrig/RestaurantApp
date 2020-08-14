231edf7652176f388e937b4b7f0b8e23e1c863a6
Denrig <bogdanboer99@gmail.com>
2020-08-14 16:59:24 +0300

    Code styled

--- app/controllers/admin_base_controller.rb
+++ app/controllers/admin_base_controller.rb
@@ -2,8 +2,6 @@ class AdminBaseController < ApplicationController
   layout 'admin'
   before_action :admin?
 
-  def index; end
-
   def admin?
     unless current_user.admin
       flash[:danger] = 'Only privilaged users can acces that page!'
