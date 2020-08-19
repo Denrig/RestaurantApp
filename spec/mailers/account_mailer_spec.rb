438e0e934b28bc69cbb20076c1acd426d89a0731
Denrig <bogdanboer99@gmail.com>
2020-08-19 15:00:34 +0300

    Wrote tests for the admin namespace

--- spec/mailers/account_mailer_spec.rb
+++ spec/mailers/account_mailer_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe AccountMailer, type: :mailer do
   describe "activation" do
-    let(:mail) { AccountMailer.activation }
+    let(:mail) { AccountMailer.activation(User.create(name: 'This dude', email: 'thisdude@mail.com', password: 'password', password_confirmation: 'password')) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq(["thisdude@mail.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
