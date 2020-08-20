fa1ccb51a0d263fc4283a6f9ed352cf05b02e558
Denrig <bogdanboer99@gmail.com>
2020-08-20 12:30:37 +0300

    Created security helper

--- app/helpers/security_helper.rb
+++ app/helpers/security_helper.rb
@@ -3,4 +3,34 @@ module SecurityHelper
   def new_token
     SecureRandom.urlsafe_base64
   end
+
+  # Returns the hash digest of the given string
+  def digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Check if we know the user already
+  def authenticated?(object, atribute, token)
+    object_token = object.send("#{atribute}_token")
+    token_time = object.send("#{atribute}_created_at")
+
+    return false if digest.nil? || expired?(token_time)
+
+    if object_token == token
+      object.send("#{atribute}=", new_token)
+      object.send("#{atribute}_created_at=", Time.zone.now)
+      true
+    else
+      false
+    end
+  end
+
+  def expired?(time)
+    time < 1.hour.ago
+  end
 end
--- db/migrate/20200820084223_rename_columns_for_users.rb
+++ db/migrate/20200820084223_rename_columns_for_users.rb
@@ -1,4 +1,8 @@
 class RenameColumnsForUsers < ActiveRecord::Migration[6.0]
   def change
+    rename_column :users, :activation_digest, :activation_token
+    add_column :users, :activation_created_at, :datetime
+    rename_column :users, :reset_digest, :reset_token
+    rename_column :users, :reset_send_at, :reset_created_at
   end
 end
